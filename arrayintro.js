c64b1ae047e6b0c0aa8c4f16756c169a41db2985
Aman Kumar <136882937+Aman-9454@users.noreply.github.com>
2024-02-03 11:37:17 +0530

    Add files via upload

---
+++ arrayintro.js
@@ -0,0 +1,12 @@
+let a = [1,2,3,4,"aman", false];
+console.log(a);
+
+let b = new Array(5, "amit",)
+console.log(b);
+/**how to access the array element ****/
+console.log(a[5]);
+/**updation in array***/
+a[4] = "vimal";
+a[3] = 10;
+a[5] = true;
+console.log(a);
