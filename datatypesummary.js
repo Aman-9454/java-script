3cec252a448cac5651e58169a78db2762dd74b97
Aman Kumar <136882937+Aman-9454@users.noreply.github.com>
2024-02-06 00:34:51 +0530

    Add files via upload

---
+++ datatypesummary.js
@@ -0,0 +1,18 @@
+// Primtive
+
+// 7types primtive: string, number, boolean, null, undefined, symboll, bigint
+
+const score = 100
+const scoreValue = 100.34
+
+const IsLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+// symboll decleration
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id===anotherid);
+// reference (non primitive)
+
+// Array, Objects, Functions
