0e5cef3aea70357690cbeeab0c8425117e301eef
Aman Kumar <136882937+Aman-9454@users.noreply.github.com>
2024-02-07 14:30:14 +0530

    Add files via upload

---
+++ comparision.js
@@ -0,0 +1,20 @@
+/**(>) is less than
+ * (<) is grater than
+ * (==) is equal to 
+ * (===) is strictly equal to 
+ * (!=) is not equal to 
+ * (>=) is less than or equal to
+ * (<=) is greater than or equal to
+ */
+
+// dangerous comparision 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+console.log("3" === 5);
