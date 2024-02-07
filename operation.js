d461d052465b8937d73311bff785defe4efdf913
Aman Kumar <136882937+Aman-9454@users.noreply.github.com>
2024-02-07 13:49:04 +0530

    Add files via upload

---
+++ operation.js
@@ -0,0 +1,22 @@
+/**********************opreation***********************/
+/*
+1. "+"
+2. "-"
+3. "*"
+4. "**" power of 
+5. "/"
+6. "%" modulus or remainder
+*/
+let aman = 199
+aman++;
+// console.log(aman);
+
+let str1 = "hello"
+let str2 = " Ashwin"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log("1" + 2 + 3);
+console.log(1 + 3 + "4");
+console.log((5+3)*3%5);
