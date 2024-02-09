38d9c4be5476c1d77d8a1cfaa9ef31d01aff097c
Aman Kumar <136882937+Aman-9454@users.noreply.github.com>
2024-02-09 22:57:16 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,20 @@
+const name = "aman kumar"
+const repoCount = 50
+
+// console.log(name +  repoCount + " value"); it is old methode
+
+console.log(`Namste mera naam ${name} hai aur mera repocount ${repoCount} hai`);//it is the new way of the syntax 
+
+const gameName = new String('amankr');
+console.log(gameName);
+console.log(gameName[0]);//way to access the key value of the string.
+console.log(gameName.__proto__);//syntax to access the object
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());//it converts the whole string into the uppercase 
+
+console.log(gameName.charAt(5));//it is use to find on the given position which char is stands
+
+const newstringOne = "     amit    ";
+console.log(newstringOne);
+console.log(newstringOne.trim());//{.trim()} removes all the spaces from the string
