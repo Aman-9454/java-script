e4e987cc6b7559ee8dc859d658365a19ed2370ad
Aman Kumar <136882937+Aman-9454@users.noreply.github.com>
2024-02-12 23:11:56 +0530

    Add files via upload

---
+++ number.js
@@ -0,0 +1,7 @@
+const score = 300
+console.log(score); // here autometically define it is a number
+
+const balance = new Number(200);
+console.log(balance); // here we specifically cast the it is a number 
+
+console.log(balance.toString()); // here data type is changing from number to string
